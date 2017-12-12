6db299ced6b7f2097d4647338c03fa622885793b
lukkan99 <lukkan99@gmail.com>
2017-12-12 14:01:55 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    type = discord.Status.online
+    game = discord.Game(name="https://www.battleforthenet.com/")
+    await bot.change_presence(status=type, game=game)
 
 @client.event
 async def on_message(message):
@@ -19,5 +22,6 @@ async def on_message(message):
         tmp = await client.send_message(message.channel, 'running command culd be slow as not on fastline at server ips')
          await asyncio.sleep(5)
         await client.edit_message(tmp, "youre ips has blocked this bot")
+        await client.send_message(message.channel, 'https://www.battleforthenet.com/')
 
 client.run(token)
