3b8039494e19520c6f013ab053d0c04ee12d8570
lukkan99 <lukkan99@gmail.com>
2017-12-12 14:18:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,15 +13,15 @@ async def on_ready():
     print('------')
     type = discord.Status.online
     game = discord.Game(name="https://www.battleforthenet.com/")
-    await bot.change_presence(status=type, game=game)
+    await client.change_presence(status=type, game=game)
 
 @client.event
 async def on_message(message):
     if message.content.startswith(prefix):
         counter = 0
-        tmp = await client.send_message(message.channel, 'running command culd be slow as not on fastline at server ips')
-         await asyncio.sleep(5)
-        await client.edit_message(tmp, "youre ips has blocked this bot")
+        tmp = await client.send_message(message.channel, 'running cmd. culd be slow as host ips slowing down connation to discord api')
+        await asyncio.sleep(5)
+        await client.edit_message(tmp, "youre ips has blocked the use this bot sorry!")
         await client.send_message(message.channel, 'https://www.battleforthenet.com/')
 
 client.run(token)
