a0677fe70e0affaec00128cc14328a2562ea5694
lukkan99 <lukkan99@gmail.com>
2017-12-12 14:24:06 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,6 +22,7 @@ async def on_message(message):
         tmp = await client.send_message(message.channel, 'running cmd. culd be slow as host ips slowing down connation to discord api')
         await asyncio.sleep(5)
         await client.edit_message(tmp, "you're ips has blocked the use this bot sorry! go look at hare current plans for discord.")
+        await asyncio.sleep(2)
         await client.send_message(message.channel, 'dont make this reallty check this out. https://www.battleforthenet.com/')
 
 client.run(token)
